9ba7a511be269acfc0042da7eb5a08d11ee4d112
nurilpakaya <nurilpakaya@gmail.com>
2024-11-04 10:47:32 +0700

    git init 03-product

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">E - Kedai
+            <a href="index.html">KEDAI NUGE
                 <img src="{{ asset('img/kedai.png') }}"
                 alt="logo"
                 width="70">
